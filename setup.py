2a0d4e4e206d2fc87fdb6d7785b7f7295fa97580
Juan Miguel Serrano Rodríguez <juan11iguel@gmail.com>
2023-08-03 17:34:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='psa_models',
+    name='models_psa',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         
     ],
     author="jmserrano",
-    author_email="jmserrnao@psa.es",
-    description="Steady state models for: a solar flat-plate collector field, heat exchanger, a thermal storage tank and a three-way valve.",
+    author_email="jmserrano@psa.es",
+    description="Repository with source code of a variety of models of a solar field - thermal storage and MED plant combined system at Plataforma Solar de Almería ",
     long_description="",
     long_description_content_type="text",
-    url="https://github.com/Juasmis/librescada",
+    url="https://github.com/juan11iguel/models_psa",
 )
