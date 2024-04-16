00e64b9d564e3e024cec37c40228e340b0de2c3b
jmserrano <jmserrano@psa.es>
2024-04-16 14:45:14 -0300

    Sync WIP

--- solarMED_optimization/visualization/__init__.py
+++ solarMED_optimization/visualization/__init__.py
@@ -17,23 +17,30 @@ colors = {
 
 }
 
-def state_evolution_plot(df, ):
+
+def state_evolution_plot(df, iteration: int = None):
 
     # Encode states to integers
 
-    if 'sf_ts_state' not in df.columns:
-        raise ValueError("Column 'sf_ts_state' not found in the DataFrame")
-    if 'med_state' not in df.columns:
-        raise ValueError("Column 'med_state' not found in the DataFrame")
+    if 'state_sf_ts' not in df.columns:
+        raise ValueError("Column 'state_sf_ts' not found in the DataFrame")
+    if 'state_med' not in df.columns:
+        raise ValueError("Column 'state_med' not found in the DataFrame")
+
+    if not isinstance(df["state_sf_ts"][0], SF_TS_State):
+        raise ValueError("The column 'state_sf_ts' must be of type SF_TS_State")
+
+    if not isinstance(df["state_med"][0], MedState):
+        raise ValueError("The column 'state_med' must be of type MedState")
 
     # sf_active = False and (ts_active = False or ts_active=True) -> 1
     # sf_active = True and ts_active = False -> 6
     # sf_active = True and ts_active = True -> 11
 
-    df["sf_ts_value"] = (1.5 *  (df["sf_ts_state"] == SF_TS_State.IDLE) +
-                         1.5 *  (df["sf_ts_state"] == SF_TS_State.RECIRCULATING_TS) +
-                         6.5 *  (df["sf_ts_state"] == SF_TS_State.HEATING_UP_SF) +
-                         11.5 * (df["sf_ts_state"] == SF_TS_State.SF_HEATING_TS))
+    df["sf_ts_value"] = (1.5 *  (df["state_sf_ts"] == SF_TS_State.IDLE) +
+                         1.5 *  (df["state_sf_ts"] == SF_TS_State.RECIRCULATING_TS) +
+                         6.5 *  (df["state_sf_ts"] == SF_TS_State.HEATING_UP_SF) +
+                         11.5 * (df["state_sf_ts"] == SF_TS_State.SF_HEATING_TS))
 
     # Assign the following labels to the states:
     # 1 -> "S̲F̲ T̲S̲"
@@ -46,25 +53,26 @@ def state_evolution_plot(df, ):
         {1.5: colors["transparent"], 6.5: colors["yellow"], 11.5: colors["yellow"]}
     )
 
-    # med_state = INACTIVE -> 3
-    # med_state = STARTING -> 8
-    # med_state = ACTIVE -> 13
-    # med_state = STOPPING -> 8
+    # state_med = INACTIVE -> 3
+    # state_med = STARTING -> 8
+    # state_med = ACTIVE -> 13
+    # state_med = STOPPING -> 8
 
-    df["med_state_value"] = (2.5 *  (df["med_state"] == MedState.OFF) +
-                             7.5 *  (df["med_state"] == MedState.IDLE) +
-                             7.5 *  (df["med_state"] == MedState.STARTING_UP) +
-                             7.5 *  (df["med_state"] == MedState.SHUTTING_DOWN) +
-                             12.5 * (df["med_state"] == MedState.ACTIVE))
+    df["state_med_value"] = (2.5 *  (df["state_med"] == MedState.OFF) +
+                             7.5 *  (df["state_med"] == MedState.GENERATING_VACUUM) +
+                             7.5 *  (df["state_med"] == MedState.IDLE) +
+                             7.5 *  (df["state_med"] == MedState.STARTING_UP) +
+                             7.5 *  (df["state_med"] == MedState.SHUTTING_DOWN) +
+                             12.5 * (df["state_med"] == MedState.ACTIVE))
 
     # Assign the following labels to the states:
     # 3 -> "M̲E̲D̲"
     # 8 -> "M̲E̲D̲"
     # 13 -> "M̅E̅D̅"
 
-    df["med_state_label"] = df["med_state_value"].map({2.5: "M̲E̲D̲", 7.5: "M̲E̲D̲", 12.5: "M̅E̅D̅"})
-    df["med_state_bg"] = df["med_state_value"].map({2.5: colors["gray"], 7.5: colors["white"], 12.5: colors["green"]})
-    df["med_state_border"] = df["med_state_value"].map(
+    df["state_med_label"] = df["state_med_value"].map({2.5: "M̲E̲D̲", 7.5: "M̲E̲D̲", 12.5: "M̅E̅D̅"})
+    df["state_med_bg"] = df["state_med_value"].map({2.5: colors["gray"], 7.5: colors["white"], 12.5: colors["green"]})
+    df["state_med_border"] = df["state_med_value"].map(
         {2.5: colors["transparent"], 7.5: colors["green"], 12.5: colors["green"]}
     )
 
@@ -93,9 +101,9 @@ def state_evolution_plot(df, ):
 
     fig.add_trace(go.Scatter(
         x=df.index,
-        y=df["med_state_value"],
+        y=df["state_med_value"],
         mode="lines",
-        name="med_state_value",
+        name="state_med_value",
         line=dict(color=colors["purple"], width=0.5),
     ))
 
@@ -116,20 +124,41 @@ def state_evolution_plot(df, ):
         ))
         annotations.append(dict(
             x=row.name,
-            y=row['med_state_value'],
-            text=row['med_state_label'],
+            y=row['state_med_value'],
+            text=row['state_med_label'],
             showarrow=False,
-            bordercolor=row['med_state_border'],
+            bordercolor=row['state_med_border'],
             borderwidth=3,
-            bgcolor=row['med_state_bg'],
+            bgcolor=row['state_med_bg'],
         ))
+
+    # Add a vertical bar in iteration if provided
+    if iteration:
+        fig.add_shape(
+            dict(
+                type="rect", # other options are "rect", "circle", "line"
+                x0=iteration-0.5,
+                x1=iteration+0.5,
+                y0=0,
+                y1=15,
+                # line=dict(
+                #     color="orange",
+                #     width=1,
+                #     dash="dashdot"
+                # )
+                fillcolor="rgba(182, 182, 182,0.1)",
+            )
+        )
 
     fig.update_layout(
         annotations=annotations,
-        title="State Machine",
+        title="<b>State Machine evolution<b>" if iteration is None else f"<b>State Machine evolution</b> - Iteration {iteration}<br>{df['state_title'][iteration]}</br>",
         xaxis_title="Time",
         yaxis_title="State",
         legend_title="Legend",
+        showlegend=False,
+        # Add some margin to the right
+        margin=dict(r=10, t=70, b=10, l=10),
         # plot_bgcolor="rgba(182, 182, 182,0.1)",
         # plot_bgcolor="rgba(182, 182, 182,0.15)",
         yaxis=dict(
