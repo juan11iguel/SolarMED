cae6f19ce8e971755005e6e10ee9138cc83b9ab7
Juan Miguel Serrano <juan11iguel@gmail.com>
2024-05-07 17:38:45 -0300

    Improve validation readability and parametized `conHotTemperatureType` upper limit

--- solarMED_modeling/data_validation.py
+++ solarMED_modeling/data_validation.py
@@ -4,7 +4,13 @@ from loguru import logger
 import numpy as np
 
 rangeType = Annotated[tuple[float, float], Field(..., min_items=2, max_items=2)]
-conHotTemperatureType = Annotated[float, Field(..., ge=0, le=120)]
+
+# Sometimes when using larger sample rates, the controller will produce great changes in the solar field outlet
+# temperature, which makes this validation to fail, that's why it's increased to 125 to give a bit more margin
+# A better alternative would be to give feedback to the controller when this happens, to re-evaluate the control action,
+# or maybe just modify the control action until the output is within limits
+conHotTemperatureType_upper_limit: float = 120.0
+conHotTemperatureType = Annotated[float, Field(..., ge=0, le=conHotTemperatureType_upper_limit)]
 
 def check_value_single(field_value, field_name):
 
@@ -24,7 +30,7 @@ def check_value_single(field_value, field_name):
 def within_range_or_make_zero(value: float, range: rangeType, var_name:str= None) -> float:
 
     if value < range[0]:
-        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]} -> 0.0")
+        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]:.2f} -> 0.0")
         return 0.0
     elif value > range[1]:
         raise ValidationError([{"loc": ["value"], "msg": f"Value {value:.2f} is greater than {range[1]}", "type": "value_error"}])
@@ -38,10 +44,10 @@ def within_range_or_zero_or_max(value: float, range: rangeType, var_name:str= No
         return 0
 
     if value < range[0]:
-        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]} -> 0.0")
+        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]:.2f} -> 0.0")
         return 0.0
     elif value > range[1]:
-        logger.debug(f"({var_name}) Value {value:.2f} is greater than {range[1]} -> {range[1]}")
+        logger.debug(f"({var_name}) Value {value:.2f} is greater than {range[1]:.2f} -> {range[1]:.2f}")
         return range[1]
     else:
         return value
@@ -49,10 +55,10 @@ def within_range_or_zero_or_max(value: float, range: rangeType, var_name:str= No
 def within_range_or_nan_or_max(value: float, range: rangeType, var_name:str= None) -> float:
 
     if value < range[0]:
-        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]} -> 0.0")
+        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]:.2f} -> 0.0")
         return np.nan
     elif value > range[1]:
-        logger.debug(f"({var_name}) Value {value:.2f} is greater than {range[1]} -> {range[1]}")
+        logger.debug(f"({var_name}) Value {value:.2f} is greater than {range[1]:.2f} -> {range[1]:.2f}")
         return range[1]
     else:
         return value
@@ -60,10 +66,10 @@ def within_range_or_nan_or_max(value: float, range: rangeType, var_name:str= Non
 def within_range_or_min_or_max(value: float, range: rangeType, var_name:str= None) -> float:
 
     if value < range[0]:
-        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]} -> {range[0]}")
+        logger.debug(f"({var_name}) Value {value:.2f} is less than {range[0]:.2f} -> {range[0]:.2f}")
         return range[0]
     elif value > range[1]:
-        logger.debug(f"({var_name}) Value {value:.2f} is greater than {range[1]} -> {range[1]}")
+        logger.debug(f"({var_name}) Value {value:.2f} is greater than {range[1]:.2f} -> {range[1]:.2f}")
         return range[1]
     else:
         return value
