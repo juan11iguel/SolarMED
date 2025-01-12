2ef470d8b3643630519c3858264a412b19f8cb1d
juan11iguel <juan11iguel@gmail.com>
2025-01-12 12:03:30 +0000

    WIP new methodology to generate good candidates for the initial population

--- src/solarmed_optimization/utils/initialization.py
+++ src/solarmed_optimization/utils/initialization.py
@@ -1,18 +1,31 @@
 from pathlib import Path
 import math
-from dataclasses import fields
+from enum import Enum
+from dataclasses import fields, asdict
+import numpy as np
 from loguru import logger
 import hjson
 import datetime
+import pygmo as pg
 
+from solarmed_optimization.problems import BaseMinlpProblem
 from solarmed_modeling.utils import data_preprocessing, data_conditioning
-from solarmed_modeling.solar_med import SolarMED
+from solarmed_modeling.solar_med import SolarMED, InitialStates
+from solarmed_modeling.fsms.med import MedFsm, FsmInputs as MedFsmInputs
+from solarmed_modeling.fsms.sfts import SolarFieldWithThermalStorageFsm, FsmInputs as SfTsFsmInputs
+from solarmed_modeling.fsms import MedState, SfTsState
 
-from solarmed_optimization import (DecisionVariablesUpdates,
+from solarmed_optimization import (DecisionVariables, 
+                                   VarIdsOptimToFsmsMapping,
+                                   DecisionVariablesUpdates,
                                    ProblemData,
                                    ProblemParameters,
                                    ProblemSamples)
-from solarmed_optimization.utils import validate_dec_var_updates, times_to_samples
+from solarmed_optimization.utils import (validate_dec_var_updates, 
+                                         times_to_samples,
+                                         decision_variables_to_decision_vector,
+                                         decision_vector_to_decision_variables,
+                                         validate_real_dec_vars)
 
 # @dataclass
 # class ProblemInitializationVars:
@@ -20,27 +33,6 @@ from solarmed_optimization.utils import validate_dec_var_updates, times_to_sampl
 #     samples_data: SampleParams
 #     model: SolarMED
 
-from dataclasses import dataclass, field
-from solarmed_modeling.solar_med import FsmInternalState
-from solarmed_modeling.fsms import MedState, SfTsState
-import numpy as np
-
-# TODO: Do not use this dataclass but the one from solarmed_modeling.solar_med
-@dataclass
-class InitialStates:
-    ## Thermal storage
-    Tts_h: np.ndarray[float]
-    Tts_c: np.ndarray[float]
-    ## Finite state machines
-    fsms_internal_states: FsmInternalState = field(default_factory=lambda: FsmInternalState())
-    med_state: MedState = MedState.OFF
-    sf_ts_state: SfTsState = SfTsState.IDLE
-    ## Solar field
-    Tsf_in_ant: np.ndarray[float] = field(default_factory=lambda: np.array([0.], dtype=float))
-    qsf_ant: np.ndarray[float] = field(default_factory=lambda: np.array([0.], dtype=float))
-    
-    def __post_init__(self):
-        self.Tts_h = np.array(self.Tts_h, dtype=float)
         
 renames_dict: dict[str, str] = {
     # First rename the original columns
@@ -125,6 +117,12 @@ def problem_initialization(problem_params: ProblemParameters, date_str: str, dat
         default_fields = {field.name: ps.default_n_dec_var_updates for field in fields(DecisionVariablesUpdates)}
         pp.dec_var_updates = DecisionVariablesUpdates(**default_fields)
         pp.dec_var_updates.qsf = math.floor(pp.optim_window_time / ((pp.sample_time_mod+pp.sample_time_opt)/2)) # Midpoint between sample_time_mod and sample_time_opt updates
+        # New, simplify MINLP by limiting the updates of operation mode variables independent of the horizon
+        # TODO: Ideally we would limit the number of updates per horizon duration (e.g. 3 updates every 18 hours)
+        pp.dec_var_updates.med_active = 6
+        pp.dec_var_updates.med_vac_state = 6
+        pp.dec_var_updates.sf_active = 6
+        pp.dec_var_updates.ts_active = 6
 
     validate_dec_var_updates(dec_var_updates=pp.dec_var_updates, optim_window_time=pp.optim_window_time, sample_time_mod=pp.sample_time_mod)
 
@@ -160,4 +158,167 @@ def problem_initialization(problem_params: ProblemParameters, date_str: str, dat
                        problem_params=problem_params, 
                        problem_samples=problem_samples, 
                        model=model)
+    
+    
+def generate_integer_pop(model: SolarMED, pp: ProblemParameters, pop_size: int) -> list[DecisionVariables]:
+    """Function that generates a population of decision variables for the optimization problem.
+    In particular, it sets the logical sequence of inputs for the MED and Solar Field with Thermal Storage FSMs in
+    order to go from the initial state ('OFF' / 'IDLE') to the desired state ('ACTIVE').
+
+    Args:
+        model (SolarMED): _description_
+        pp (ProblemParameters): _description_
+        pop_size (int): _description_
+
+    Returns:
+        _type_: _description_
+        
+    Example:
+        pop_size = 24
+        shifted_active, shifted_vacuum = generate_shifted_sequences(model, pp, pop_size)
+        # print(f"{shifted_active=}")
+        print(f"{shifted_vacuum=}")
+        Current result (first sequence does not repeat)
+        shifted_vacuum=array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 2, 2, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 2, 2, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 2, 2, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 2, 2, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 2, 2, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 2, 2, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 2, 2, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+            [2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 2, 2, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 2, 2, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 2, 2, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 2, 2, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 2, 2, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 2, 2, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 2, 2, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+            [2, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 2, 2, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 2, 2, 1, 1, 1, 1, 1, 1]
+        ])
+    """
+    
+    fsms = [
+        MedFsm(
+            sample_time=pp.sample_time_opt,
+            initial_state=model.med_state,
+            params=model.fsms_params.med,
+            internal_state=model.fsms_internal_states.med
+        ),
+        SolarFieldWithThermalStorageFsm(
+            sample_time=pp.sample_time_opt,
+            initial_state=model.sf_ts_state,
+            params=model.fsms_params.sf_ts,
+            internal_state=model.fsms_internal_states.sf_ts
+        )
+    ]
+    
+    # Initialize the FSM and base arrays
+    input_pops = {}
+    for fsm, FsmInputsCls, desired_state in zip(fsms, [MedFsmInputs, SfTsFsmInputs], [MedState.ACTIVE, SfTsState.SF_HEATING_TS]):
+        step_idx = 0
+        n_updates = getattr(pp.dec_var_updates, fields(FsmInputsCls)[0].name)
+        inputs = {fld.name: np.empty((n_updates, ), dtype=fld.type if not issubclass(fld.type, Enum) else int) 
+                 for fld in fields(FsmInputsCls)}
+
+        # Generate base input arrays
+        
+        # Use max() to find the member with the highest value
+        input_values = {}
+        for fld in fields(FsmInputsCls):
+            # Either are Enums or booleans
+            if issubclass(fld.type, Enum):
+                input_values[fld.name] = max(fld.type, key=lambda e: e.value)
+            else:
+                input_values[fld.name] = True
+                
+        while fsm.state != desired_state and step_idx < n_updates:
+            # fsm.step(fsm.states_inputs_set[ desired_state.name ])
+            fsm.step(FsmInputsCls(**input_values))
+            expected_inputs = fsm.get_inputs_for_current_state()
+
+            for input_name, input_val in asdict(expected_inputs).items():
+                input_val = input_val if not isinstance(input_val, Enum) else input_val.value
+                inputs[input_name][step_idx] = input_val
+            
+            step_idx += 1
+
+        if step_idx < n_updates:
+            # Fill the rest of the arrays with the last value
+            for input_name, input_val in asdict(expected_inputs).items():
+                input_val = input_val if not isinstance(input_val, Enum) else input_val.value
+                inputs[input_name][step_idx:] = input_val
+
+        # Create shifted arrays with repetition
+        inputs_pop = {input_name: np.zeros((pop_size, n_updates), dtype=type(input_value[0]) if not isinstance(type(input_value[0]), Enum) else int) 
+                     for input_name, input_value in inputs.items()}
+
+        for i in range(pop_size-1):
+            shift = i % n_updates  # Cyclic index for repetition
+            if shift == 0:
+                # First row is the base
+                for input_name, input_val in inputs.items():
+                    inputs_pop[input_name][i+1] = input_val
+            else:
+                # Shift the base array to the right and set the first `shift` elements to 0
+                for input_name, input_val in inputs.items():
+                    inputs_pop[input_name][i+1] = np.roll(input_val, shift)
+                    inputs_pop[input_name][i+1, :shift] = 0
+        
+        input_pops.update(**inputs_pop)
+        
+    # print(f"{input_pops=}")
+    
+    return [
+        DecisionVariables(
+            **{VarIdsOptimToFsmsMapping(name).name: value[pop_idx] for name, value in input_pops.items()},
+        
+            qsf=np.nan * np.ones((pp.dec_var_updates.qsf, )),
+            qts_src=np.nan * np.ones((pp.dec_var_updates.qts_src, )),
+            qmed_s=np.nan * np.ones((pp.dec_var_updates.qmed_s, )),
+            qmed_f=np.nan * np.ones((pp.dec_var_updates.qmed_f, )),
+            Tmed_c_out=np.nan * np.ones((pp.dec_var_updates.Tmed_c_out, )),
+            Tmed_s_in=np.nan * np.ones((pp.dec_var_updates.Tmed_s_in, )),
+        ) 
+        for pop_idx in range(pop_size)]
+    
+    
+def generate_population(model: SolarMED, pp: ProblemParameters, 
+                        problem: BaseMinlpProblem, pop_size: int,
+                        return_decision_vector: bool = False,
+                        dec_vec: np.ndarray = None, 
+                        prob: pg.problem = None) -> list[DecisionVariables] | np.ndarray:
+    
+    assert dec_vec is not None or prob is not None, "Either a starting population is provided (dec_vec), or a pg.problem instance is provided (prob)"
+    
+    # Generate integer population based on FSMs logic starting from current state
+    pop_dec_vars = generate_integer_pop(model, pp, pop_size)
+    
+    for i, dec_vars in enumerate(pop_dec_vars):
+        
+        # Either initialize random decision vectors
+        # Or use the decision vector provided
+        if dec_vec is None:
+            dec_vec = pg.random_decision_vector(prob)
+
+        dec_vars_ = decision_vector_to_decision_variables(dec_vec, dec_var_updates=problem.dec_var_updates, span='none', )
+        
+        # Set the real decision variables values in the manually generated decision variables
+        [setattr(dec_vars, var_id, getattr(dec_vars_, var_id)) for var_id in problem.dec_var_real_ids]
+        # Validate the real decision variables values using the manually established logical variables values
+        dec_vars = validate_real_dec_vars(dec_vars, problem.real_dec_vars_box_bounds)
+        
+    if not return_decision_vector:
+        return pop_dec_vars
+    
+    return np.array([decision_variables_to_decision_vector(dec_vars) for dec_vars in pop_dec_vars])
     
